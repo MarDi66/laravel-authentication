a9ee0595a5855e05f308db7f65f36df2b799778c
MarDi66 <yohann.mtfpro@gmail.com>
2019-12-09 10:36:20 +0100

    Add link to create listing and home

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Create new listing</div>
+                <div class="card-header">Create new listing <span class="float-right"><a href="/home" class="btn btn-secondary">Go back</a></span></div>
 
                 <div class="card-body">
                     @if (session('status'))
